f9f02a728e4c84d23e5add3b164f5099d8d3a9a7
Pravalika2007 <thandrapravalika.24.ece@anits.edu.in>
2026-03-15 23:09:46 +0530

    Add files via upload

---
+++ mymath.py
@@ -0,0 +1,55 @@
+class MyMath:
+
+    def calculate(self, operation, n):
+
+        if operation == "sum":
+            total = sum(range(1, n + 1))
+            print("Sum =", total)
+
+        elif operation == "prime":
+            count = 0
+            num = 2
+            while count < n:
+                for i in range(2, int(num**0.5) + 1):
+                    if num % i == 0:
+                        break
+                else:
+                    print(num, end=" ")
+                    count += 1
+                num += 1
+            print()
+
+        elif operation == "fibonacci":
+            a, b = 0, 1
+            for i in range(n):
+                print(a, end=" ")
+                a, b = b, a + b
+            print()
+
+        elif operation == "factorial":
+            fact = 1
+            for i in range(1, n + 1):
+                fact *= i
+            print("Factorial =", fact)
+
+
+math = MyMath()
+
+print("1. Sum of first n natural numbers")
+print("2. First n prime numbers")
+print("3. Fibonacci series")
+print("4. Factorial")
+
+choice = int(input("Enter choice: "))
+n = int(input("Enter value of n: "))
+
+if choice == 1:
+    math.calculate("sum", n)
+elif choice == 2:
+    math.calculate("prime", n)
+elif choice == 3:
+    math.calculate("fibonacci", n)
+elif choice == 4:
+    math.calculate("factorial", n)
+else:
+    print("Invalid choice")
